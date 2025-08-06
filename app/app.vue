24115c75ab0a8ddab6fff082ee8871b0280d5b2f
Hiromichi NOMATA <git@hiromichinomata.com>
2025-08-07 00:52:40 +0900

    add: sidebar

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="dark">
-    <h1 class="text-red-500 dark:text-dim-500">Twitter clone</h1>
+<div :class="{ 'dark' : darkMode }">
+    <div class="min-h-full">
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+        <!-- Left sidebar -->
+        <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft></SidebarLeft>
+            </div>
+        </div>
+
+        <!-- Main content -->
+        <main class="col-span-12 md:col-span-8 xl:col-span-6">
+          sdf
+        </main>
+
+        <!-- Right Sidebar -->
+        <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+            <div class="sticky top-0">
+              dfg
+            </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+import { SidebarLeft } from '#components';
+
+  const darkMode = ref(true)
+</script>
---
+++ app/components/Sidebar/Left.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>Left sidebar</div>
+</template>
+<script setup>
+</script>
