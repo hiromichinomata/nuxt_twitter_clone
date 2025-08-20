d2c7a51ef7689fc981397a1b2cfcbdd7789c1bda
Hiromichi NOMATA <git@hiromichinomata.com>
2025-08-21 02:06:34 +0900

    add: defaultTransition

--- app/components/Sidebar/Left.vue
+++ app/components/Sidebar/Left.vue
@@ -1,11 +1,14 @@
 <template>
-    <div>
-        <nuxt-link to="/">
-            <div class="w-8 h-8">
-                <LogoTwitter />
-            </div>
-        </nuxt-link>
+    <div class="flex flex-col h-screen">
+        <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20" :class="defaultTransition">
+            <nuxt-link to="/">
+                <div class="w-8 h-8">
+                    <LogoTwitter />
+                </div>
+            </nuxt-link>
+        </div>
 </div>
 </template>
 <script setup>
+const { defaultTransition } = useTailwindConfig()
 </script>
