62b07cea6d174840698e80f1a7d4ffef89b97b4a
Hiromichi NOMATA <git@hiromichinomata.com>
2025-08-24 01:29:33 +0900

    fix: login

--- app/composables/useAuth.js
+++ app/composables/useAuth.js
@@ -1,4 +1,5 @@
-import jwt_decode from "jwt-decode"
+import { jwtDecode } from "jwt-decode"
+import useFetchApi from "./useFetchApi.js"
 
 export default () => {
     const useAuthToken = () => useState('auth_token')
@@ -74,7 +75,7 @@ export default () => {
             return
         }
 
-        const jwt = jwt_decode(authToken.value)
+        const jwt = jwtDecode(authToken.value)
 
         const newRefreshTime = jwt.exp - 60000
 
