6be2b88131b08a5d977147cb8375ce4b71b434c9
Hiromichi NOMATA <git@hiromichinomata.com>
2025-08-24 19:39:16 +0900

    add: TweetForm

--- app/components/Tweet/Details.vue
+++ app/components/Tweet/Details.vue
@@ -2,8 +2,8 @@
     <div>
         <TweetItem :tweet="props.tweet" />
 
-        <!-- <TweetForm placeholder="Tweet your reply" :reply-to="props.tweet" :user="props.user"
-            @on-success="handleFormSuccess" /> -->
+        <TweetForm placeholder="Tweet your reply" :reply-to="props.tweet" :user="props.user"
+            @on-success="handleFormSuccess" />
 
         <TweetListFeed :tweets="replies" />
     </div>
