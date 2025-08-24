9ab364af6f820a8f59fea1a33749547e81b8cba0
Hiromichi NOMATA <git@hiromichinomata.com>
2025-08-24 22:17:00 +0900

    add: search bar

--- app/components/Sidebar/Right/index.vue
+++ app/components/Sidebar/Right/index.vue
@@ -1,6 +1,20 @@
 <template>
     <div clas="flex flex-col">
 
+        <!-- Search bar -->
+        <div class="relative m-2">
+            <div class="absolute flex items-center h-full pl-4 text-gray-600 cursor-pointer">
+                <div class="w-6 h-6">
+                    <SearchIcon @click="handleSearch" />
+                </div>
+            </div>
+            <input v-model="search"
+                class="flex items-center w-full pl-12 text-sm font-normal text-black bg-gray-200 border border-gray-200 rounded-full shadow dark:text-gray-100 dark:bg-dim-400 dark:border-dim-400 focus:bg-gray-100 dark:focus:bg-dim-900 focus:outline-none focus:border focus:border-blue-200 h-9"
+                placeholder="Search tweet" type="text">
+        </div>
+
+
+        <!-- Preview Card : What's happening -->
         <SidebarRightPreviewCard title="What's happening">
 
             <SidebarRightPreviewCardItem v-for="whatsHappening in whatsHappeningItems">
@@ -15,6 +29,8 @@
 
         </SidebarRightPreviewCard>
 
+
+        <!-- Preview Card : Who to follow -->
         <SidebarRightPreviewCard title="Who to follow">
             <SidebarRightPreviewCardItem v-for="whoToFollow in whoToFollowItems">
                 <div class="flex flex-row items-center justify-between p-2">
@@ -33,10 +49,50 @@
                 </div>
             </SidebarRightPreviewCardItem>
         </SidebarRightPreviewCard>
+
+
+        <footer>
+            <ul class="mx-2 my-4 text-xs text-gray-500">
+                <li class="inline-block mx-2">
+                    <a href="#" class="hover:underline" @click.prevent="handleToggleDarkMode">Dark mode</a>
+                </li>
+                <li class="inline-block mx-2">
+                    <a href="#" class="hover:underline">Privacy Policy</a>
+                </li>
+                <li class="inline-block mx-2">
+                    <a href="#" class="hover:underline">Cookie Policy</a>
+                </li>
+                <li class="inline-block mx-2">
+                    <a href="#" class="hover:underline">Accessability</a>
+                </li>
+                <li class="inline-block mx-2">
+                    <a href="#" class="hover:underline">Ads info</a>
+                </li>
+                <li class="inline-block mx-2">
+                    <a href="#" class="hover:underline">More</a>
+                </li>
+                <li class="inline-block mx-2">
+                    © 2022 Twitter, Inc.
+                </li>
+            </ul>
+        </footer>
+
+
     </div>
 </template>
 <script setup>
 import { SearchIcon } from '@heroicons/vue/outline';
+const search = ref('')
+const emitter = useEmitter()
+
+function handleSearch() {
+    useRouter().push({
+        path: '/search',
+        query: {
+            q: search.value
+        }
+    })
+}
 
 const whatsHappeningItems = ref([
     {
@@ -70,5 +126,9 @@ const whoToFollowItems = ref([
         image: 'https://picsum.photos/200/200'
     }
 ])
+
+function handleToggleDarkMode() {
+    emitter.$emit('toggleDarkMode')
+}
 
 </script>
